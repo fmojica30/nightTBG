b8911f6a55a261f0ed8b43392c74bc36c4e7f180
Fernando Mojica <fernmarmojica@gmail.com>
2022-08-03 21:01:58 -0500

    Added in functionality to see where input is being clicked

--- CanvasListener.js
+++ CanvasListener.js
@@ -3,8 +3,16 @@ class CanvasListener {
   }
    
   init() {
-    var e = document.getElementById("gameContainer");
-    e.addEventListener('click', function(event) {
+    var canvas = document.getElementById("gameContainer");
+    canvas.addEventListener('click', function(event) {
+      const rect = canvas.getBoundingClientRect();
+      var xVal = event.clientX - rect.left;
+      var yVal = event.clientY - rect.top;
+      console.log(xVal, yVal);
+
+      if (xVal > utils.withGridScaled(5) && xVal < utils.withGridScaled(6) && yVal > utils.withGridScaled(5) && yVal < utils.withGridScaled(6)) {
+        console.log("Party Leader");
+      }
     });
   }
 }
