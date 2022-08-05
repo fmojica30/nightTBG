54168e02b269c10e7e3a435939309df2a7b3cde5
Fernando Mojica <fernmarmojica@gmail.com>
2022-08-05 18:15:13 -0500

    Add in menu styling and Hero class

--- HeroOptionMenu.js
+++ HeroOptionMenu.js
@@ -1,5 +1,5 @@
 class HeroOptionMenu {
-  constructor({hero, onComplete}) {
+  constructor({ hero, onComplete }) {
     this.hero = hero;
     this.onComplete = onComplete;
     this.element = null;
@@ -8,18 +8,17 @@ class HeroOptionMenu {
   createElement() {
     this.element = document.createElement("div");
     this.element.classList.add("heroMenu");
-    this.element.innerHTML = (`
+    this.element.innerHTML = `
     <p class="TextMessage_p">${this.text}</p>
     <button class ="TextMessage_button">Next</button> 
-    `);
+    `;
     this.element.querySelector("button").addEventListener("click", () => {
       console.log("button");
-    })
+    });
   }
 
   init(container) {
     this.createElement();
     container.appendChild(this.element);
   }
-
 }
