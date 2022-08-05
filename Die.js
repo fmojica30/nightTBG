17715e66d79451ce28d9521e19ceb9ec9173363f
slimas11 <slimas11@users.noreply.github.com>
2022-08-05 12:28:11 -0500

    Format structure of code

--- Die.js
+++ Die.js
@@ -1,51 +1,43 @@
 class Die {
-    constructor(config){
-        this.id = 'die';
+	constructor(config) {
+		this.id = 'die';
 
-        this.currentNumber = 6;
-        this.x = config.x || 0;
-        this.y = config.y || 0;
-        this.sprite = new Sprite({
-            gameObject: this,
-            imageSrc: config.imageSrc || "assets/dice/die-six.png",
-            adjustX: config.adjustX || 0,
-            adjustY: config.adjustY || 0,
-            animateNow: config.animateNow || 1,
-            currentAnimation: config.currentAnimation || "die",
-          });
-    }
+		this.currentNumber = 6;
+		this.x = config.x || 0;
+		this.y = config.y || 0;
+		this.sprite = new Sprite({
+			gameObject: this,
+			imageSrc: config.imageSrc || 'assets/dice/die-six.png',
+			adjustX: config.adjustX || 0,
+			adjustY: config.adjustY || 0,
+			animateNow: config.animateNow || 1,
+			currentAnimation: config.currentAnimation || 'die',
+		});
+	}
 
-    /* Updates the face value on screen 
+	/* Updates the face value on screen 
     Need to fix face value method as it parses through 'dieSheet'
     */
-      updateFaceValue(){
-       switch(this.currentNumber){
-           case 1:
-                this.sprite.imageSrc = "assets/dice/die-six.png";
-                break;
-            case 2:
-                
-                break;
-            case 3:
-                
-                break;
-            case 4:
-                
-                break;
-            case 5:
-                
-                break;
-            case 6:
-                
-                break;
-        }
-    }
+	updateFaceValue() {
+		switch (this.currentNumber) {
+			case 1:
+				this.sprite.imageSrc = 'assets/dice/die-six.png';
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+		}
+	}
 
-    rollDie (){
-        this.currentNumber = Math.floor(Math.random()*6)+1;
-        updateFaceValue();
-    }
-
-    
-
+	rollDie() {
+		this.currentNumber = Math.floor(Math.random() * 6) + 1;
+		updateFaceValue();
+	}
 }
--- Sprite.js
+++ Sprite.js
@@ -1,79 +1,112 @@
 class Sprite {
-  constructor(config) {
-    //Might need to create multiple images for animations 
-    this.image = new Image();
-    this.image.src = config.imageSrc;
-    this.image.onload = () => {
-      this.isLoaded = true;
-    };
-    this.gameObject = config.gameObject;
+	constructor(config) {
+		//Might need to create multiple images for animations
+		this.image = new Image();
+		this.image.src = config.imageSrc;
+		this.image.onload = () => {
+			this.isLoaded = true;
+		};
+		this.gameObject = config.gameObject;
 
-    this.adjustX = config.adjustX || 0;
-    //If sprite is taller than 16px this moves it down to match the tile its supposed to be on
-    this.adjustY = config.adjustY || 0;
+		this.adjustX = config.adjustX || 0;
+		//If sprite is taller than 16px this moves it down to match the tile its supposed to be on
+		this.adjustY = config.adjustY || 0;
 
-    //Sprite sheet animation setup
-    this.animations = config.animation || {
-      "idle": [[0,0], [1,0], [2,0], [3,0]],
-      "selected": [[0,1], [1,1], [2,1], [3,1]],
-      "die":[[0,0]],
-    }
+		//Sprite sheet animation setup
+		this.animations = config.animation || {
+			idle: [
+				[0, 0],
+				[1, 0],
+				[2, 0],
+				[3, 0],
+			],
+			selected: [
+				[0, 1],
+				[1, 1],
+				[2, 1],
+				[3, 1],
+			],
+			die: [[0, 0]],
+		};
 
-    this.currentAnimation = config.currentAnimation || "idle";
-    this.currentAnimationFrame = 0;
-    this.animationFrameLimit = config.animationFrameLimit || 3;
-    this.animationFrameProgress = this.animationFrameLimit;
+		this.currentAnimation = config.currentAnimation || 'idle';
+		this.currentAnimationFrame = 0;
+		this.animationFrameLimit = config.animationFrameLimit || 3;
+		this.animationFrameProgress = this.animationFrameLimit;
 
-    this.frameCounter = 0;
-    this.animateNow = config.animateNow;
-  }
+		this.frameCounter = 0;
+		this.animateNow = config.animateNow;
+	}
 
-  get frame() {
-    return this.animations[this.currentAnimation][this.currentAnimationFrame];
-  } 
+	get frame() {
+		return this.animations[this.currentAnimation][this.currentAnimationFrame];
+	}
 
-  setAnimation(key) {
-    if (this.currentAnimation !== key) {
-      this.currentAnimation = key;
-      this.currentAnimationFrame = 0;
-      this.animationFrameProgress = this.animationFrameLimit;
-    }
-  }
+	setAnimation(key) {
+		if (this.currentAnimation !== key) {
+			this.currentAnimation = key;
+			this.currentAnimationFrame = 0;
+			this.animationFrameProgress = this.animationFrameLimit;
+		}
+	}
 
-  updateAnimationProgress() {
-    //Downtick frame progress
-    if (this.animationFrameProgress > 0) {
-      this.animationFrameProgress -= 1;
-      return;
-    }
+	updateAnimationProgress() {
+		//Downtick frame progress
+		if (this.animationFrameProgress > 0) {
+			this.animationFrameProgress -= 1;
+			return;
+		}
 
-    //Reset the counter
-    this.animationFrameProgress = this.animationFrameLimit;
-    this.currentAnimationFrame += 1;
+		//Reset the counter
+		this.animationFrameProgress = this.animationFrameLimit;
+		this.currentAnimationFrame += 1;
 
-    if (this.frame === undefined) {
-      this.currentAnimationFrame = 0;
-    }
-  }
+		if (this.frame === undefined) {
+			this.currentAnimationFrame = 0;
+		}
+	}
 
-  update() {
-    if (this.frameCounter === this.animateNow) {
-      this.updateAnimationProgress();
-      this.frameCounter = 0;
-    } else {
-      this.frameCounter += 1;
-    }
-  }
+	update() {
+		if (this.frameCounter === this.animateNow) {
+			this.updateAnimationProgress();
+			this.frameCounter = 0;
+		} else {
+			this.frameCounter += 1;
+		}
+	}
 
-  draw(ctx) {
-    //Customizations work for 32x32 sprite sheet
-    const x = this.gameObject.x + this.adjustX;
-    const y = this.gameObject.y + this.adjustY;
+	draw(ctx) {
+		//Customizations work for 32x32 sprite sheet
+		const x = this.gameObject.x + this.adjustX;
+		const y = this.gameObject.y + this.adjustY;
 
-    const [frameX, frameY] = this.frame;
-    if(this.currentAnimation === "die"){
-    this.isLoaded && ctx.drawImage(this.image, frameX * 16, frameY * 16, 16, 16, x, y, 16, 16);
-    }else{
-    this.isLoaded && ctx.drawImage(this.image, frameX * 32, frameY * 32, 32, 32, x, y, 32, 32);
-    }}
+		const [frameX, frameY] = this.frame;
+		if (this.currentAnimation === 'die') {
+			this.isLoaded &&
+				ctx.drawImage(
+					this.image,
+					frameX * 16,
+					frameY * 16,
+					16,
+					16,
+					x,
+					y,
+					16,
+					16
+				);
+		} else {
+			this.isLoaded &&
+				ctx.drawImage(
+					this.image,
+					frameX * 32,
+					frameY * 32,
+					32,
+					32,
+					x,
+					y,
+					32,
+					32
+				);
+		}
+	}
 }
